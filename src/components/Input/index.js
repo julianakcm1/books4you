8a1b75ceb5bb4ce44ed7026afc04d06b1bd9cb86
julianakcm1 <jkcm@ic.ufal.br>
2024-12-29 00:29:21 -0300

    feat: search and input components

---
+++ src/components/Input/index.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: 1px solid #FFF;
+  background: transparent;
+  border: 1px solid #FFF;
+  padding: 10px 70px;
+  border-radius: 50px;
+  width: 250px;
+  color: #FFF;
+  font-size: 16px;
+  margin-bottom: 10px;
+
+  &::placeholder {
+    color: #FFF;
+    font-size: 16px;
+    text-align: center;
+  }
+`
+
+export default Input;
